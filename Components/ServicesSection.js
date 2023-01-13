c9ed02b4d8e486f913066d7b27d9c1b1caf22ec3
ameya-6964 <belvalkar7@gmail.com>
2023-01-14 00:50:43 +0530

    Added Svg and Icons

---
+++ Components/ServicesSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>Quality</span>Services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
